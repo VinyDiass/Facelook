5be3eb24ab21cdce8adc77532aca9abee41a0dbc
Viny Dias <vinybrother@yahoo.com.br>
2019-07-28 16:10:24 -0300

    Estrutura HTML

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Facelook - Cosméticos</title>
+</head>
+<body>
+    <!-- HEADER -->
+    <header>
+    <!-- NAVEGAÇÃO -->
+        <nav>
+            <!-- LOGO -->
+            <h1>
+                <img src="" alt="">
+            </h1>
+            <!-- ITEMS MENU -->
+            <div>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+            </div>
+            <!-- MENU IDIOMAS -->
+            <nav>
+                <a href="#"></a>
+                <div>
+                    <a href=""></a>
+                    <a href=""></a>
+                </div>
+            </nav>
+        </nav>
+    </header>
+
+    <main>
+        <!-- SLIDER -->
+        <section>
+            <div>
+                <div>
+                    <h2>Exemplo titulo página</h2>
+                    <p> Conteudo de paragrafo </p>
+                    <a href="#">Revender</a>
+                </div>
+            </div>
+        </section>
+        <!-- LINHAS DE PRODUTOS -->
+        <section>
+            <header>
+                <h2>Linhas</h2>
+                <h3>Veja nossa linha</h3>
+            </header>
+            <!-- AREA CARDS -->
+            <div>
+            <!-- CARD -->
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+            <!-- CARD -->
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+            <!-- CARD -->
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </section>
+        <!-- CONHEÇA A FACELOOK -->
+        <section>
+            <div>
+                <div style="background-image: url();"></div>
+            </div>
+            <div>
+                <h2></h2>
+                <div>
+                    <p></p>
+                </div>
+            </div>
+        </section>
+        <!-- LIGA DOS BARBER -->
+        <section>
+            <header>
+                <h2>Conheça a liga dos barber </h2> 
+                <p></p>
+            </header>
+            <!-- AREA BARBEIROS -->
+            <div>
+                <!-- BOX BARBEIRO -->
+                <div class="imagemBarbeiro" style="background-image:url();">
+                    <div class="conteudoBarbeiro">
+                        <h4></h4>
+                        <h5></h5>
+                        <p></p>
+                        <nav>
+                            <a href=""></a>
+                            <a href=""></a>
+                            <a href=""></a>
+                            <a href=""></a>
+                        </nav>
+                    </div>
+                </div>
+                <!-- BOX BARBEIRO -->
+                <div class="imagemBarbeiro" style="background-image:url();">
+                    <div class="conteudoBarbeiro">
+                        <h4></h4>
+                        <h5></h5>
+                        <p></p>
+                        <nav>
+                            <a href=""></a>
+                            <a href=""></a>
+                            <a href=""></a>
+                            <a href=""></a>
+                        </nav>
+                    </div>
+                </div>
+                <!-- BOX BARBEIRO -->
+                <div class="imagemBarbeiro" style="background-image:url();">
+                    <div class="conteudoBarbeiro">
+                        <h4></h4>
+                        <h5></h5>
+                        <p></p>
+                        <nav>
+                            <a href=""></a>
+                            <a href=""></a>
+                            <a href=""></a>
+                            <a href=""></a>
+                        </nav>
+                    </div>
+                </div>
+                <!-- FIM AREA BARBEIROS -->
+            </div>
+        </section>
+        <!-- NEWSLETTER -->
+        <section>
+            <!-- AREA CONTEUDO NEWSLETTER -->
+            <div>
+                <div>
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div>
+                    <input type="text" name="newsletter">
+                    <button>Enviar</button>
+                </div>
+            </div>
+        </section>
+
+        <!-- BLOG -->
+        <section>
+            <!-- AREA BLOG -->
+            <div>
+                <!-- POST -->
+                <div>
+                    <div>
+                        <i></i><a href="#"></a>
+                        <h4></h4>
+                        <p></p>
+                        <a href="#">Leia mais...</a>
+                    </div>
+                </div>
+                <!-- POST -->
+                <div>
+                    <div>
+                        <i></i><a href="#"></a>
+                        <h4></h4>
+                        <p></p>
+                        <a href="#">Leia mais...</a>
+                    </div>
+                </div>
+                <!-- POST -->
+                <div>
+                    <div>
+                        <i></i><a href="#"></a>
+                        <h4></h4>
+                        <p></p>
+                        <a href="#">Leia mais...</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- AREA REDES SOCIAIS -->
+        <div>
+            <nav>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </nav>
+        </div>
+    </main>
+    <!-- FOOTER -->
+    <footer>
+    <div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+    <div>
+        <div>
+            <p>Facelook 2019</p>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
